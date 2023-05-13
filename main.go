5a7d1b0fc6083280a4361109f100a1e9e9f8df64
Igor Diakonov <aidos.tanatos@gmail.com>
2023-05-13 14:08:19 +0300

    Server receives messages from client

--- main.go
+++ main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"embed"
-	"io/fs"
-    log "github.com/sirupsen/logrus"
-	"net/http"
-    "github.com/gobwas/ws"
+	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
+	log "github.com/sirupsen/logrus"
+	"io/fs"
+	"net"
+	"net/http"
 )
 
 //go:embed frontend/dist/*
 var frontendFiles embed.FS
 
-func wsHandler(w http.ResponseWriter, r *http.Request){
-    conn, _, _, err := ws.UpgradeHTTP(r, w)
-	if err != nil {
-		// handle error
-        log.Error("Upgrade HTTP: ", err)
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	var conn net.Conn
+	var err error
+	if r.Header["Connection"][0] == "Upgrade" {
+		conn, _, _, err = ws.UpgradeHTTP(r, w)
+		if err != nil {
+			// handle error
+			log.Error("Upgrade HTTP: ", err)
+		}
+	} else {
+		log.Error("Wrong Connection header: ", r.Header["Connection"])
+		return
 	}
 	go func() {
 		defer conn.Close()
@@ -25,22 +33,22 @@ func wsHandler(w http.ResponseWriter, r *http.Request){
 			msg, op, err := wsutil.ReadClientData(conn)
 			if err != nil {
 				// handle error
-                log.Error("Read: ", err)
-                break
+				log.Error("Read: ", err)
+				break
 			}
-            log.Info("Message: ", string(msg))
+			log.Info("Message: ", string(msg))
 			err = wsutil.WriteServerMessage(conn, op, msg)
 			if err != nil {
 				// handle error
-                log.Error("Write: ", err)
-                break
+				log.Error("Write: ", err)
+				break
 			}
 		}
 	}()
 }
 
 func main() {
-    http.Handle("/socket.io/", http.HandlerFunc(wsHandler))
+	http.Handle("/socket.io", http.HandlerFunc(wsHandler))
 	fs, _ := fs.Sub(frontendFiles, "frontend/dist")
 	http.Handle("/", http.FileServer(http.FS(fs)))
 
