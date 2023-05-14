02e657b459e4b1e68e764a0685df9f32bfd24ee1
Igor Diakonov <aidos.tanatos@gmail.com>
2023-05-14 16:45:59 +0300

    Moved loading to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,43 @@ import './App.css';
 import Chat from './Chat';
 
 function App() {
+  // send a GET request to the server when the page loads
+  window.addEventListener("load", () => {
+    fetch(window.location.origin + "/params")
+      .then(response => response.json())
+      .then(data => {
+        const wsProtocol = data['wsProtocol'];
+        const wsHost = data['wsHost'];
+        const wsPort = data['wsPort'];
+        const wsPath = data['wsPath'];
+        const wsAddress = `${wsProtocol}://${wsHost}:${wsPort}${wsPath}`;
+        const ws = new WebSocket(wsAddress);
+        ws.onopen = () => {
+          console.log('Connected to server');
+        };
+        ws.onerror = (error) => {
+          console.log('WebSocket error: ', error);
+        };
+        ws.onclose = () => {
+          console.log('Disconnected from server');
+        };
+        // render the Chat component with the ws prop
+        return (
+          <div className="App">
+              <Chat ws={ws} />
+          </div>
+        );
+      })
+      .catch(error => {
+        // handle any errors that occur
+        console.error(error);
+      });
+  });
   return (
-    <div className="App">
-        <Chat />
-    </div>
-  );
+  <div className="App">
+      <Chat />
+  </div>
+  )
 }
 
 export default App;
--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -2,41 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom';
 import './Chat.css';
 
-// send a GET request to the server when the page loads
-window.addEventListener("load", () => {
-  fetch(window.location.origin + "/params")
-    .then(response => response.json())
-    .then(data => {
-      const wsProtocol = data['wsProtocol'];
-      const wsHost = data['wsHost'];
-      const wsPort = data['wsPort'];
-      const wsPath = data['wsPath'];
-      const wsAddress = `${wsProtocol}://${wsHost}:${wsPort}${wsPath}`;
-      const ws = new WebSocket(wsAddress);
-      ws.onopen = () => {
-        console.log('Connected to server');
-      };
-      ws.onerror = (error) => {
-        console.log('WebSocket error: ', error);
-      };
-      ws.onclose = () => {
-        console.log('Disconnected from server');
-      };
-      // render the Chat component with the ws prop
-      createRoot(document.getElementById('root')).render(<Chat ws={ws} />);
-    })
-    .catch(error => {
-      // handle any errors that occur
-      console.error(error);
-    });
-});
-
 function Chat(props) {
   const { ws } = props;
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState('');
 
   useEffect(() => {
+    if (!ws) return;
     ws.onmessage = (event) => {
         const message = JSON.parse(event.data);
         setMessages((prevMessages) => [...prevMessages, message]);
@@ -45,7 +17,7 @@ function Chat(props) {
     return () => {
       ws.close();
     };
-  }, []);
+  }, [ws]);
 
   function handleInputChange(event) {
     setText(event.target.value);
