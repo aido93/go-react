e33a2fb052ab90febe587cf8552579905b358e58
Igor Diakonov <aidos.tanatos@gmail.com>
2023-05-15 20:22:37 +0300

    Trying to add auto-reconnect

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,64 +1,57 @@
 import React, { useState, useEffect } from 'react';
-import { createRoot } from 'react-dom';
 import './Chat.css';
 
 function Chat(props) {
   const { wsAddress } = props;
-  const [ws, setWs] = useState(new WebSocket(wsAddress));
-  const [wsClosed, setWsClosed] = useState(false);
+  const [ws, setWs] = useState(null);
+  const [wsClosed, setWsClosed] = useState(true);
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState('');
 
   useEffect(() => {
-    if (!ws) return;
-    checkWSClosed();
-    return () => {
-      ws.close();
-    };
+    if (!ws) {
+      connectToWebSocket();
+    }
   }, [ws]);
 
   function handleInputChange(event) {
     setText(event.target.value);
   }
-  function checkWSClosed () {
-    if (wsClosed) {
-        setWs(new WebSocket(wsAddress));
-        setWsClosed(false);
-    }
-    ws.onopen = () => {
-        console.log('Connected to server');
-        setWsClosed(false);
+
+  async function connectToWebSocket() {
+    const newWs = new WebSocket(wsAddress);
+    newWs.onopen = () => {
+          console.log('Connected to server');
+          setWs(newWs);
+          setWsClosed(false);
     };
-    ws.onerror = (error) => {
-        console.log('WebSocket error: ', error);
-        setWsClosed(true);
+    newWs.onerror = (error) => {
+          console.log('WebSocket error: ', error);
+          setWsClosed(true);
     };
-    ws.onclose = () => {
-        console.log('Disconnected from server');
-        setWsClosed(true);
+    newWs.onclose = () => {
+          console.log('Disconnected from server');
+          setWsClosed(true);
     };
-    ws.onmessage = (event) => {
-        const message = JSON.parse(event.data);
-        setMessages((prevMessages) => [...prevMessages, message]);
+    newWs.onmessage = (event) => {
+          const message = JSON.parse(event.data);
+          setMessages((prevMessages) => [...prevMessages, message]);
     };
   }
-  const handleSubmit = (event) => {
-    event.preventDefault();
+
+  function sendWebSocketMessage() {
     if (text.trim() === '') return;
     const message = { text };
-    checkWSClosed();
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.send(JSON.stringify(message));
-      setText('');
-    } else {
-      const interval = setInterval(() => {
-      if (ws.readyState === WebSocket.OPEN) {
-          clearInterval(interval);
-          ws.send(JSON.stringify(message));
-          setText('');
-        }
-      }, 1000);
+    ws.send(JSON.stringify(message));
+    setText('');
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (wsClosed || ws.readyState !== WebSocket.OPEN) {
+      await connectToWebSocket();
     }
+    sendWebSocketMessage();
   };
 
   return (
